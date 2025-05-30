e1a32c111ea6beb13dbd97bd529ba89e6c25d576
Anurag Sharma <anuragmessi5@gmail.com>
2025-05-30 11:01:42 +0530

    implement use

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,26 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Spades","Diamonds", "Clubs"];
-    let values = ["Ace", "Two", "Three"];
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = ["Ace", "Two", "Three"];
 
-    let mut cards = vec![];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
-    }
 
-    let deck = Deck { cards };
+        Deck { cards } // returned implicitly
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
 
     println!("Here's your deck: {:#?}", deck);
 }
